d318735eef8da2724fdab5fd8b8b08e928dc5eae
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-11-20 10:26:33 +0700

    Create genesis block

--- src/test.js
+++ src/test.js
@@ -3,24 +3,4 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-const previousBlockHash = '1siub1uo23hioq23o12';
-const currentBlockData = [
-  {
-    amount: 10,
-    sender: '123saudihuaisd',
-    recipient: '12312hsad'
-  },
-  {
-    amount: 30,
-    sender: '123saudihuaisd',
-    recipient: '12312hsad'
-  },
-  {
-    amount: 50,
-    sender: '123saudihuaisd',
-    recipient: '12312hsad'
-  }
-];
-
-const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+console.log(bitcoin);
